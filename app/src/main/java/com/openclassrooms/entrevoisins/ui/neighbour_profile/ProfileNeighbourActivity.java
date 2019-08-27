9611ca0c1922a21123722f9df62e51a3b0c158ee
Fpoittevin <dev@francoispoittevin.Fr>
2019-08-27 10:23:51 +0200

    Création du FavoriteButton sur ProfileNeighbourActivity

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_profile/ProfileNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_profile/ProfileNeighbourActivity.java
@@ -1,7 +1,10 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_profile;
 
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,6 +12,7 @@ import com.bumptech.glide.Glide;
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.model.Neighbour;
+import com.openclassrooms.entrevoisins.service.FavoriteNeighboursManager;
 import com.openclassrooms.entrevoisins.service.NeighbourApiService;
 
 import butterknife.BindView;
@@ -18,12 +22,16 @@ public class ProfileNeighbourActivity extends AppCompatActivity {
 
     private Neighbour mNeighbour;
     private NeighbourApiService mNeighbourApiService;
+    private FavoriteNeighboursManager mFavoriteNeighboursManager;
+    private boolean neighbourIsFavorite;
 
     //UI Components
     @BindView(R.id.activity_profile_neighbour_avatar_img)
     ImageView mAvatar;
     @BindView(R.id.activity_profile_neighbour_name_txt)
     TextView mName;
+    @BindView(R.id.activity_profile_neighbour_favorite_btn)
+    FloatingActionButton mFavoriteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,7 @@ public class ProfileNeighbourActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_neighbour);
 
         mNeighbourApiService = DI.getNeighbourApiService();
+        mFavoriteNeighboursManager = new FavoriteNeighboursManager(this);
 
         //Generate mNeighbour with id in Intent
         int idNeighbour = getIntent().getIntExtra("idNeighbour", 0);
@@ -38,9 +47,33 @@ public class ProfileNeighbourActivity extends AppCompatActivity {
 
         //Insert neighbour data in layout
         ButterKnife.bind(this);
-
         mName.setText(mNeighbour.getName());
-
         Glide.with(this).load(mNeighbour.getAvatarUrl()).into(mAvatar);
+
+        //Favorite Button
+        setColorFavoriteButton();
+
+        mFavoriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (neighbourIsFavorite) {
+                    mFavoriteNeighboursManager.removeToFavorite(mNeighbour);
+                } else {
+                    mFavoriteNeighboursManager.addToFavorite(mNeighbour);
+                }
+                setColorFavoriteButton();
+            }
+        });
+    }
+
+    private void setColorFavoriteButton() {
+        neighbourIsFavorite = mFavoriteNeighboursManager.isFavorite(mNeighbour);
+
+        if (neighbourIsFavorite) {
+            mFavoriteButton.setColorFilter(Color.YELLOW);
+        } else {
+            mFavoriteButton.setColorFilter(Color.WHITE);
+        }
     }
 }
