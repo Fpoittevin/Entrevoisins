41a289040f70ed0547e5b40b3d73a5dcc2d10bf1
Fpoittevin <dev@francoispoittevin.Fr>
2019-09-20 11:31:11 +0200

    Ajout des attributs manquants dans la classe Neighbour ainsi que dans le neighbourGenerator

--- app/src/main/java/com/openclassrooms/entrevoisins/model/Neighbour.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/model/Neighbour.java
@@ -16,6 +16,23 @@ public class Neighbour {
     /** Avatar */
     private String avatarUrl;
 
+    /** Address */
+    private String address;
+
+    /** Distance */
+    private float distance;
+
+    /** Phone number */
+    private String phoneNumber;
+
+    /** Facebook account */
+    private String facebookAccount;
+
+    /** Description */
+    private String description;
+
+
+
     /** Favorite */
     private boolean isFavorite;
 
@@ -24,11 +41,22 @@ public class Neighbour {
      * @param id
      * @param name
      * @param avatarUrl
+     * @param address
+     * @param distance
+     * @param phoneNumber
+     * @param facebookAccount
+     * @param description
      */
-    public Neighbour(Integer id, String name, String avatarUrl) {
+    public Neighbour(Integer id, String name, String avatarUrl, String address, float distance, String phoneNumber, String facebookAccount, String description) {
         this.id = id;
         this.name = name;
         this.avatarUrl = avatarUrl;
+        this.address = address;
+        this.distance = distance;
+        this.phoneNumber = phoneNumber;
+        this.facebookAccount = facebookAccount;
+        this.description = description;
+        this.isFavorite = false;
     }
 
     public Integer getId() {
@@ -55,11 +83,29 @@ public class Neighbour {
         this.avatarUrl = avatarUrl;
     }
 
+    public String getAddress() { return address; }
+
+    public void setAddress(String address) { this.address = address; }
+
+    public float getDistance() { return distance; }
+
+    public void setDistance(float distance) { this.distance = distance; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public String getFacebookAccount() { return facebookAccount; }
+
+    public void setFacebookAccount(String facebookAccount) { this.facebookAccount = facebookAccount; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
     public boolean isFavorite() { return isFavorite; }
 
-    public void setFavorite(boolean favorite) {
-        isFavorite = favorite;
-    }
+    public void setFavorite(boolean favorite) { isFavorite = favorite; }
 
     @Override
     public boolean equals(Object o) {
